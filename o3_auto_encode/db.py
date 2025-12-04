353d37fc7dc5be350bf03133b651dd13e0d12aa0
André Strømsnes Helland <andrehelland99@gmail.com>
2025-12-04 13:21:29 +0100

    fix import

--- o3_auto_encode/db.py
+++ o3_auto_encode/db.py
@@ -3,11 +3,10 @@
 import json
 from pathlib import Path
 
-import utils
 import yaml
 from tqdm import tqdm
 
-from o3_auto_encode import logger
+from o3_auto_encode import logger, utils
 from o3_auto_encode.enums import BundleStatus
 from o3_auto_encode.file_manager import Bundle, Clip
 
